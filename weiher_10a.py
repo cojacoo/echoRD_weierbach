0cda54132ee1cb5720dec940282f395fec8c1f37
cojack <conrad.jackisch@kit.edu>
2017-05-31 13:45:12 +0200

    w10

---
+++ weiher_10a.py
@@ -0,0 +1,2 @@
+import echoRD_job as E
+E.echoRD_job(mcinif='mcini_weierbach_z10',mcpick='weierbach_z10.pickle3',runname='weiherbach_z10',wdir='/beegfs/work/ka_oj4748/echoRD_weierbach',pathdir='../echoRD/')
