acf72e61c7f9b78cc7ff5b50b62aa692933ce234
Conrad Jackisch <jackisch@kit.edu>
2018-03-30 07:42:32 +0200

    new Y05 runs with varying macpore capacity scaling added

---
+++ BinAc_weiher_hdf2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=120:00:00
+#PBS -l mem=48gb
+#PBS -S /bin/bash
+#PBS -N echoRD_weiher_hdf2
+#PBS -j oe
+#PBS -o LOG_weiher_hdf
+#PBS -n
+
+#cd /beegfs/work/ka_oj4748
+
+echo "User:"
+whoami
+echo "Job running on node:"
+uname -a
+echo "started on "
+date
+echo "- - - - - - - - - - -\n"
+echo "Weiherbach with HDF5"
+echo "- - - - - - - - - - -\n"
+
+cd /beegfs/work/ka_oj4748/echoRD_weierbach
+export PATH=$PATH:$HOME/miniconda3/bin/
+#source activate test_h5py
+
+$HOME/miniconda3/bin/python weierbach_Y05a_binac.py &
+$HOME/miniconda3/bin/python weierbach_Y05b_binac.py &
+$HOME/miniconda3/bin/python weierbach_Y05c_binac.py &
+$HOME/miniconda3/bin/python weierbach_Y05d_binac.py
---
+++ weierbach_Y05b_binac.py
@@ -0,0 +1,14 @@
+
+mcinif='mcini_weierbach_Y05'
+mcpick='weierbach_Y05.pickle3'
+runname='weiherbach_Y05b'
+wdir='/beegfs/work/ka_oj4748/echoRD_weierbach'
+pathdir='../echoRD/'
+
+import sys
+from pathlib import Path
+import numpy as np
+sys.path.append(pathdir)
+
+import run_echoRD as rE
+rE.echoRD_job(mcinif=mcinif, mcpick=mcpick, runname=runname, wdir=wdir, pathdir=pathdir, hdf5pick=True, update_md_area=5., fsize=(2.35,4),w_rat=[3.5,0.6],h_rat=[0.8,9])
